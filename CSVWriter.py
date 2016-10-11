2a63c21526d0c77ae6ed3ab5381325f1a7962e4d
dbullock1086 <dbullock1086@users.noreply.github.com>
2016-10-11 08:58:28 -0500

    header changed to comma-separated for h5py access

--- CSVWriter.py
+++ CSVWriter.py
@@ -1,27 +1,21 @@
+#!/usr/bin/env python                                                                                                        
+
 import numpy as np
 
-def CSVWriter(filename,X,Y,R):
-    
-    names=X.dtype.names
+def CSVWriter (filename, X, Y, R):
+    names = X.dtype.names
+    colnames = []
 
-    colnames=""
-
-    for n in names:
-        colnames+=n+"/f:"
-
-#    colnames+="TrueClass/f:"
-    for i in xrange(0,Y.shape[1]):
-        colnames+="true_"+str(i)+"/f:"
-
-    for i in xrange(0,R.shape[1]):
-        colnames+="predict_"+str(i)+"/f:"
+    for n in names: colnames.append(n)
+    for i in xrange(0, Y.shape[1]): colnames.append('true_' + str(i))
+    for i in xrange(0, R.shape[1]): colnames.append('predict_' + str(i))
 
     f = open(filename, 'w')
-    f.write(colnames[:-1]+"\n")
+    f.write(','.join(colnames) + '\n')
 
-    X0=X.view(np.float32).reshape(X.shape + (-1,))
+    X0 = X.view(np.float32).reshape(X.shape + (-1,))
+    #YI = np.nonzero(Y)[1] # DB: why is this needed?                                                                         
+    out = np.concatenate((X0,Y,R), axis=1)
 
-    YI=np.nonzero(Y)[1]
-    out=np.concatenate((X0,Y,R),axis=1)
-
-    np.savetxt(f,out,delimiter=',')
+    np.savetxt(f, out, delimiter=',')
+    pass
