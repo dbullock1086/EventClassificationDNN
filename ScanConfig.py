53e785978fe847d3d9b86089656c868501df695f
dbullockphd <daniel.bullock.phd@gmail.com>
2016-10-27 09:30:30 -0500

    regroup

--- ScanConfig.py
+++ ScanConfig.py
@@ -40,25 +40,34 @@ for F in Files:
 
 #Previously in InputVars.py
 # Select Variables To use in training
-FieldGroups = [    
+FieldGroups = [
+    # energy scale
     ['mP', 'mC', 'mX',
      'METx', 'METy',
-     'L1_M', 'L1_pT', 'L1_phi', 'L1_eta',
-     'L2_M', 'L2_pT', 'L2_phi', 'L2_eta',
-     'B1_M', 'B1_pT', 'B1_phi', 'B1_eta',
-     'B2_M', 'B2_pT', 'B2_phi', 'B2_eta',
+     'L1_M', 'L2_M', 'B1_M', 'B2_M',
+     'L1_pT', 'L2_pT', 'B1_pT', 'B2_pT'
      'MPP_AA', 'Eb_a_AA', 'Eb_b_AA', 'El_a_AA', 'El_b_AA',
      'MPP_AB', 'Eb_a_AB', 'Eb_b_AB', 'El_a_AB', 'El_b_AB',
      'MPP_BA', 'Eb_a_BA', 'Eb_b_BA', 'El_a_BA', 'El_b_BA',
-     'MPP_BB', 'Eb_a_BB', 'Eb_b_BB', 'El_a_BB', 'El_b_BB',
-     'cosPP_AA', 'cosPa_AA', 'cosPb_AA', 'cosCa_AA', 'cosCb_AA',
+     'MPP_BB', 'Eb_a_BB', 'Eb_b_BB', 'El_a_BB', 'El_b_BB'],
+
+    # transverse angle
+    ['L1_phi', 'L2_phi', 'B1_phi', 'B2_phi'],
+
+    # pseudorapidity
+    ['L1_eta', 'L2_eta', 'B1_eta', 'B2_eta'],
+
+    # decay angle
+    ['cosPP_AA', 'cosPa_AA', 'cosPb_AA', 'cosCa_AA', 'cosCb_AA',
      'cosPP_AB', 'cosPa_AB', 'cosPb_AB', 'cosCa_AB', 'cosCb_AB',
      'cosPP_BA', 'cosPa_BA', 'cosPb_BA', 'cosCa_BA', 'cosCb_BA',
-     'cosPP_BB', 'cosPa_BB', 'cosPb_BB', 'cosCa_BB', 'cosCb_BB',
-     'dphi_PP_Pa_AA', 'dphi_PP_Pb_AA', 'dphi_Pa_Ca_AA', 'dphi_Pb_Cb_AA',
+     'cosPP_BB', 'cosPa_BB', 'cosPb_BB', 'cosCa_BB', 'cosCb_BB'],
+
+    # separation angle
+    ['dphi_PP_Pa_AA', 'dphi_PP_Pb_AA', 'dphi_Pa_Ca_AA', 'dphi_Pb_Cb_AA',
      'dphi_PP_Pa_AB', 'dphi_PP_Pb_AB', 'dphi_Pa_Ca_AB', 'dphi_Pb_Cb_AB',
      'dphi_PP_Pa_BA', 'dphi_PP_Pb_BA', 'dphi_Pa_Ca_BA', 'dphi_Pb_Cb_BA',
-     'dphi_PP_Pa_BB', 'dphi_PP_Pb_BB', 'dphi_Pa_Ca_BB', 'dphi_Pb_Cb_BB']
+     'dphi_PP_Pa_BB', 'dphi_PP_Pb_BB', 'dphi_Pa_Ca_BB', 'dphi_Pb_Cb_BB'],
 ]
 
 SelectedFields = [
